a6e798256cea6dd7e67b060298830c84455a5dcc
ran232003 <45311381+ran232003@users.noreply.github.com>
2022-04-22 11:35:44 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
+import { Route, Routes } from 'react-router-dom';
+import Employess from './pages/Employess';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className = "back">
+      <NavigationBar/>
+      <Routes>
+      <Route exact  path = "/employees" element = {<Employess/>}>
+
+      </Route>
+      </Routes>
     </div>
   );
 }
