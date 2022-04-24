ab3c6b44c32fec8e94902883824c303ea70de537
ran232003 <45311381+ran232003@users.noreply.github.com>
2022-04-24 18:21:14 +0300

    pdate

---
+++ src/dummyEmployeesTaks.js
@@ -0,0 +1,41 @@
+export const employeesArrayTasks = [{
+    name:"ran",
+    lastName:"far",
+    position:"software developer",
+    id:1,
+    image:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    manager:"Adi Sahar",
+    managerId:3,
+    tasks:[{
+        text:"task1",
+        date:"31-03-1989"
+    },{
+        text:"task1",
+        date:"31-03-1989"
+    },{
+        text:"task1",
+        date:"31-03-1989"
+    }],
+    subordinates:[{
+        name:"ran2",
+        position:"software dev",
+        id:2
+    },
+    {
+        name:"ran2",
+        position:"software dev",
+        id:3
+    }]
+},{
+    name:"ran2",
+    lastName:"far2",
+    position:"software developer",
+    id:2,
+    image:"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    manager:"Adi Sahar2",
+    tasks:[{
+        text:"task12",
+        date:"31-03-1989"
+    }]
+
+}];
